dde656da4318d035ab5f4a2cf006089599f763d0
Mateusz Witek <51757549+wazash@users.noreply.github.com>
2022-04-09 15:35:39 +0200

    Implemented enemy shooting

--- sda-project2d/Assets/Scripts/Enemy/Enemy.cs
+++ sda-project2d/Assets/Scripts/Enemy/Enemy.cs
@@ -12,14 +12,38 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] private float speed;
 
+    [SerializeField] private float minInterval, maxInterval;
+
+    private float timer = -1f;
+
     private void Awake()
     {
         healthSystem.OnHealthDepleted += HealthSystem_OnHealthDepleted;
+
+        timer = Random.Range(minInterval, maxInterval);
     }
 
     private void OnDestroy()
     {
         healthSystem.OnHealthDepleted -= HealthSystem_OnHealthDepleted;
+    }
+
+    private void Update()
+    {
+        if(timer <= 0)
+        {
+            timer = Random.Range(minInterval, maxInterval);
+            Shoot();
+        }
+
+        timer -= Time.deltaTime;
+    }
+
+    private void Shoot()
+    {
+        Bullet createdBullet = Instantiate<Bullet>(bulletPrefab, transform.position, Quaternion.identity);
+
+        createdBullet.Shoot(Vector3.left);
     }
 
     private void FixedUpdate()
